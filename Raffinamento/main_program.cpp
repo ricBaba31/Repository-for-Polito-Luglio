5be24f93de920dd1b1b8299010f70b6181f5f3f0
umbertomorzone <127257161+umbertomorzone@users.noreply.github.com>
2023-07-06 15:32:25 +0200

    Update main_program.cpp

--- Raffinamento/main_program.cpp
+++ Raffinamento/main_program.cpp
@@ -22,32 +22,32 @@ int main()
 
     vector<Project::Cell2D> vectSupp;
 
-    unsigned int numbercell0D = 0;
+    //unsigned int numbercell0D = 0;
     vector<Project::Cell0D> vectp;
-    Project::ImportCell0Ds(vectp, numbercell0D, "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell0Ds.csv");
+    Project::ImportCell0Ds(vectp, "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell0Ds.csv");
 
-    unsigned int numbercell1D = 0;
+    //unsigned int numbercell1D = 0;
     vector<Project::Cell1D> vects;
     //vector<double> LengthEdges;
-    Project::ImportCell1Ds(vects, numbercell1D, vectp, "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell1Ds.csv");
+    Project::ImportCell1Ds(vects,  "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell1Ds.csv");
 
-    unsigned int numbercell2D = 0;
+    //unsigned int numbercell2D = 0;
     std::vector<vector<unsigned int>> LenghtMax;
     vector<Project::Cell2D> vectt;
     vector<Project::Cell0D> vectp2D;
-    Project::ImportCell2Ds(vectt, numbercell2D, vectp, vectp2D, "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell2Ds.csv");
+    Project::ImportCell2Ds(vectt,  vectp,  "C:/Users/utente/Desktop/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell2Ds.csv");
 
     //Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
 
-    Project::TriangularMesh mesh = Project::TriangularMesh(numbercell0D, vectp, numbercell1D, vects, numbercell2D, LenghtMax, vectt);
-    vector<vector<unsigned int>> Matr = MatrAdiac(vectt, vects);
+    Project::TriangularMesh mesh = Project::TriangularMesh(vectp, vects, vectt);
+    vector<vector<unsigned int>> Matr = MatrAdiac(mesh.vecttMesh, mesh.vectsMesh);
     //Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(vectt, vects);
 
-    for (int iter = 0; iter < 4; iter++) {
-        Sorting::HeapSort(vectSupp, vectt);
-        for (int i = 0; i < 2; i++) {
-            Project::Cell2D* trisupp = &vectt[vectSupp[i].Id2D];
-            Project::Bisect(trisupp, vectp, vects, vectt, Matr);
+    for (int iter = 0; iter < 20; iter++) {
+        Sorting::HeapSort(vectSupp, mesh.vecttMesh);
+        for (int i = 0; i < 20; i++) {
+            Project::Cell2D* trisupp = &mesh.vecttMesh[vectSupp[i].Id2D];
+            Project::Bisect(trisupp, mesh.vectpMesh, mesh.vectsMesh, mesh.vecttMesh, Matr);
         }
     }
 
